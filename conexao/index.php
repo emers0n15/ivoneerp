c491a4f43c3f820ddd0429f0fb1a25f5226469d2
Emers0n15 <emersoncovane23@gmail.com>
2025-12-01 08:22:51 +0000

    Correcao de erros #4

--- conexao/index.php
+++ conexao/index.php
@@ -1,4 +1,19 @@
 <?php
-$db = mysqli_connect('crbhlspv_ivoneerptest','root','','ivoneerp') 
-    or die("<h3 style='text-align:center;margin-top: 50px;'>Nao e possível se conectar ao banco de dados</h3>");
+// config/conexao.php
+
+$dbHost = 'localhost';            // Host do MySQL
+$dbUser = 'crbhlspv_ivoneerp';    // Usuário do banco
+$dbPass = '@Sinaboy123**@';       // Senha do usuário
+$dbName = 'crbhlspv_ivoneerp';    // Nome do banco
+
+// Conectar ao banco
+$mysqli = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+
+// Checar conexão
+if (!$mysqli) {
+    die("<h3 style='text-align:center;margin-top:50px;'>Não é possível se conectar ao banco de dados</h3>");
+}
+
+// Definir charset para evitar problemas com acentuação
+mysqli_set_charset($mysqli, "utf8mb4");
 ?>
