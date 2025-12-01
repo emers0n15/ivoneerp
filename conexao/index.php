0a1764922f9da23d93e85c3d697ff362561c5ba0
Emers0n15 <emersoncovane23@gmail.com>
2025-12-01 08:53:11 +0000

    Correcao de erros #8

--- conexao/index.php
+++ conexao/index.php
@@ -1,6 +1,11 @@
 <?php
 // config/conexao.php
 
+// Ativa exibição de erros para qualquer arquivo que inclua esta conexão
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $dbHost = 'localhost';            // Host do MySQL
 $dbUser = 'crbhlspv_ivoneerptest';    // Usuário do banco
 $dbPass = '@Sinaboy123**@';       // Senha do usuário
